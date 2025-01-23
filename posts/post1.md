887771c99519bf9cd1a1b5bb9a48a237313c82df
kimtaejin3 <rlaxowls1316@naver.com>
2025-01-23 22:57:13 +0900

    feat: add today-feel post

--- posts/post1.md
+++
@@ -1,6 +0,0 @@
----
-title: My First Post
-date: 2025-01-23
----
-
-Ullamco et nostrud magna commodo nostrud ...
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ function PostCard(post: Post) {
         {format(parseISO(post.date), "LLLL d, yyyy")}
       </time>
       <div
-        className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
+        className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0 line-clamp-1"
         dangerouslySetInnerHTML={{ __html: post.body.html }}
       />
     </div>
@@ -26,9 +26,7 @@ export default function Home() {
 
   return (
     <div className="">
-      <h1 className="mb-8 text-center text-2xl font-black">
-        Next.js + Contentlayer Example
-      </h1>
+      <h1 className="mb-8 text-center text-2xl font-black">My Diary</h1>
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
       ))}
