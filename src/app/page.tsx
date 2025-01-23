c2bc8958570da3dde8c6977b52a349a86101368b
kimtaejin3 <rlaxowls1316@naver.com>
2025-01-23 20:59:19 +0900

    design: 공통 레이아웃 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
   );
 
   return (
-    <div className="mx-auto max-w-xl py-8">
+    <div className="">
       <h1 className="mb-8 text-center text-2xl font-black">
         Next.js + Contentlayer Example
       </h1>
