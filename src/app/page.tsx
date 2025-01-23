c2b577e8e77232904487dcd96203f48d92d4abd6
kimtaejin3 <rlaxowls1316@naver.com>
2025-01-23 21:02:13 +0900

    feat: 블로그 글 상세 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,7 @@ function PostCard(post: Post) {
   return (
     <div className="mb-8">
       <h2 className="mb-1 text-xl">
-        <Link
-          href={post.url}
-          className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
-        >
-          {post.title}
-        </Link>
+        <Link href={post.url}>{post.title}</Link>
       </h2>
       <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
         {format(parseISO(post.date), "LLLL d, yyyy")}
