5958675fd518275887362a6df8cc67673f86781a
kimtaejin3 <rlaxowls1316@naver.com>
2025-01-30 22:47:13 +0900

    fix: sun, moon icon 색상 변경

--- src/components/darkmode-toggle-button.tsx
+++ src/components/darkmode-toggle-button.tsx
@@ -22,8 +22,8 @@ export function DarkModeToggleButton() {
       size="icon"
       onClick={handleThemeToggle}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-red-400" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-yellow-400" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
